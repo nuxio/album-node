fd7073bc002abc78905f840a8c0c74651a303dc6
Mafee <mafee@xiaoman.cn>
2019-05-22 17:14:14 +0800

    Add some packages

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
+
+// X-Response-Time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", ms);
+});
+
+// response
+
+router.get("/", ctx => {
+  ctx.body = "Hello world!";
+});
+
+app.use(router.routes());
 
 app.listen(3000);
